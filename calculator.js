18bc25c4933e98a0c0752ea44703a3a874655e3d
JustineMiko <justinemarzec@gmail.com>
2020-12-11 14:58:38 +0100

    Suites de l'exercice calculatrice
    Amélioration du code pour intégrer les éléments au DOM

--- calculator.js
+++ calculator.js
@@ -1,64 +1,36 @@
 'use strict'
+export default class Calculator {
+    constructor() {
+        // élément de div qui encadre tote la calculette
+        const calculatorDiv = document.createElement('div');
+        calculatorDiv.classList.add('calculatorborder', 'container');
+        body.append(calculatorDiv);
+        const body = document.body;
 
-
-export class MyCalculator {
-        constructor() {
-        const classicButton = document.createElement('button');
-        classicButton.classList.add('button-square','col-md-3');
-        const maDiv = document.createElement('div').
-        maDiv.classList.add('container');
-        document.body.append(maDiv);
-        const button = document.createElement('button');
-        button.textContent = "1";
-        maDiv.append(button);
         
-        }
+        //élément div qui encadre la barre preview:
+        const previewDiv = document.createElement('div');
+        previewDiv.classList.add('row');
+        calculatorDiv.append(previewDiv);
+
+        //élément div de prévisualisation du résultat:
+        const previewBar = document.createElement('div');
+        previewBar.classList.add('result', 'col-md-12');
+        previewDiv.append(previewBar);
+
+        //élément div : les boutons opérateurs sauf le =
+        const operatorButton = document.createElement('div');
+        operatorButton.classList.add('operator', 'row');
+        calculatorDiv.append(operatorButton);
         
-    }     
-        
+        //élément button +
+        const operatorPlus = document.createElement('button');
+        operatorButton.classList.add('button-square', 'col-md-3');
+        operatorButton.append(operatorPlus);
 
-
-// CREATION DE MES ELEMENTS
-
-// visualiseur de résultats 
-const previewBar = document.createElement('div');
-previewBar.classList.add('result', 'col-md-12');
-
-//les autres boutons sauf AC : 
-const classicButton = document.createElement('button');
-classicButton.classList.add('button-square','col-md-3')
-
-//bouton AC :
-
-const acButton = document.createElement('button');
-acButton.classList.add('button-square_AC', 'col-md-3');
-
-//bouton égal :
-const equalButton = document.createElement('button');
-equalButton.classList.add('equal', 'col-md-3');
-
-// POSITIONNEMENT DES ELEMENTS :
-
-// visualiseur de résultats :
-// const body = document.body;
-document.body.append(previewBar);
-
-const body = document.createElement(‘div’);
-body.textContent = "";
-
-// les autres boutons :
-// // const button = document.div;
-// document.div.append(classicButton, acButton, equalButton);
-// const button = document.createElement('div');
-// div.textContent = "7";
-
-
-// const maDiv = document.createElement('div').
-// maDiv.classList.add('container');
-// document.body.append(maDiv);
-// const button = document.createElement('button');
-// button.textContent = "1";
-// maDiv.append(button);
+    
+    }  
+}
 
 
 
